833da68f55c9ca1c1770f84436c0bb20af221a11
BerniceMlangeni <bernicemlangeni@gmail.com>
2024-06-05 09:03:58 +0200

    Add project files.

---
+++ C#Basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Basics
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Type in your name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine( "Hello, " + name + "!");
+
+        }
+    }
+}
